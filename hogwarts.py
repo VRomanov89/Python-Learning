a5142b930a998a4bad7fec72e93c648ab93d57c4
Vladimir Romanov <vladromweb@gmail.com>
2023-07-05 16:18:58 -0400

    Dictionaries
    Dictionaries

--- hogwarts.py
+++ hogwarts.py
@@ -5,3 +5,25 @@ for student in students:
 
 for i in range(len(students)):
     print(i + 1, students[i])
+
+# Dictionary - keys / values
+
+students2 = {
+    "Hermoine": "Gryff", 
+    "Harry": "Gryff",
+    "Draco": "Slyth",
+}
+
+print(students2["Hermoine"])
+
+for student in students2:
+    print(student, students2[student])
+
+students3 = [
+    {"name": "Hermoine", "house": "Gryff", "patron": "Otter"},
+    {"name": "Harry", "house": "Gryff", "patron": "Stag"},
+    {"name": "Draco", "house": "Slyth", "patron": None}
+]
+
+for student in students3:
+    print(student["name"], student["house"], student["patron"], sep=", ")
