c64f2a06d923684aef043a084579b7de6642e0f1
Vladimir Romanov <vladromweb@gmail.com>
2023-07-09 16:19:12 -0400

    Sorting dict
    Sorting dict

--- CS50/students.py
+++ CS50/students.py
@@ -9,10 +9,11 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {}
-        student["name"] = name
-        student["house"] = house
+        student = {"name": name, "house": house}
         students.append(student)
 
-for student in students:
+def get_house(student):
+    return student["house"]
+    
+for student in sorted(students, key=get_house):
     print(f"{student['name']} is in {student['house']}")
