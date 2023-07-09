d7d561f145f80db4cefaf1a6e96b7fdb5fee134e
Vladimir Romanov <vladromweb@gmail.com>
2023-07-09 15:30:06 -0400

    Reading CSV
    Reading CSV

--- CS50/FileIO.py
+++ CS50/FileIO.py
@@ -6,4 +6,34 @@ for _ in range(3):
 
 for name in sorted(names):
     print(f"hello, {name}")
+
+name = input("What is your name? ")
+
+with open("names.txt", "a") as file:
+    file.write(f"{name}\n")
+
+with open("names.txt", "r") as file:
+    for line in file:
+        print("hello,", line.rstrip())
+
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names):
+    print(f"Hello, {name}")
+
+with open("names.txt") as file:
+    for line in sorted(file):
+        print(f"Hello, {line}")
 """
+names = []
+
+with open("names.txt") as file:
+    for line in file:
+        names.append(line.rstrip())
+
+for name in sorted(names, reverse=True):
+    print(f"Hello, {name}")
