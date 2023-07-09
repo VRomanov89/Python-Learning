f249a189e2c62cdd04e492c95134e1eec7ba7f13
Vladimir Romanov <vladromweb@gmail.com>
2023-07-09 13:08:32 -0400

    Pytest testing
    Pytest testing

---
+++ test_calc.py
@@ -0,0 +1,39 @@
+"""
+from UnitTests import square
+
+def main():
+    test_square()
+
+def test_square():
+    try:
+        assert square(2) == 4
+    except AssertionError:
+        print("2 squared was not 4")
+    try:
+        assert square(3) == 9
+    except AssertionError:
+        print("3 squared was not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared was not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared was not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared was not 0")
+
+if __name__ == "__main__":
+    main()
+"""
+from UnitTests import square
+
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
