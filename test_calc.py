c1d2c4ca5b2475ab5995170bc45fd508a2ca1a58
Vladimir Romanov <vladromweb@gmail.com>
2023-07-09 13:31:55 -0400

    Init - File IO
    Init - File IO

--- test_calc.py
+++ test_calc.py
@@ -31,9 +31,16 @@ if __name__ == "__main__":
 """
 from UnitTests import square
 
-def test_square():
+def test_positive():
     assert square(2) == 4
     assert square(3) == 9
     assert square(-2) == 4
     assert square(-3) == 9
+    assert square(0) == 0
+
+def test_negative():
+    assert square(-2) == 4
+    assert square(-3) == 9
+
+def test_zero():
     assert square(0) == 0
