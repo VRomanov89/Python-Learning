9b94c6186c99cd6fb7974d295ff94e930678bb01
Vladimir Romanov <vladromweb@gmail.com>
2023-07-10 16:13:08 -0400

    Init - Regular Expressions
    Init - Regular Expressions

--- CS50/students.py
+++ CS50/students.py
@@ -4,13 +4,14 @@ with open("students.csv") as file:
         name, house = line.rstrip().split(",")
         print(f"{name} is in {house}")
 """
+import csv
+
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name, "home": reader})
     
 for student in sorted(students, key=lambda student: student["name"]):
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is in {student['home']}")
