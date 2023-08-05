e4bc759c3f2964809a2263fed3f2282af58f3736
Vladimir Romanov <vladromweb@gmail.com>
2023-08-05 16:36:56 -0400

    Finalized basic API in Flask
    Finalized basic API in Flask

--- PythonTutorials/API/main.py
+++ PythonTutorials/API/main.py
@@ -1,1 +1,28 @@
 #Tutorial - https://www.youtube.com/watch?v=zsYIw6RXjfM
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/get-user/<user_id>")
+def get_user(user_id):
+    user_data = {
+        "user_id": user_id,
+        "name": "John Doe",
+        "email": "john.doe@gmail.com"
+    }
+
+    extra = request.args.get("extra")
+    if extra:
+        user_data["extra"] = extra
+
+    return jsonify(user_data), 200
+
+@app.route("/create-user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    
+    return jsonify(data), 201
+
+if __name__ == "__main__":
+    app.run(debug=True)
