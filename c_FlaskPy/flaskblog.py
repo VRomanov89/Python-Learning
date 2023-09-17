65e0d9386b2125af8c17b295bdd5574a94d811ce
Vladimir Romanov <vladromweb@gmail.com>
2023-09-17 13:55:57 -0400

    Init
    Init

--- c_FlaskPy/flaskblog.py
+++ c_FlaskPy/flaskblog.py
@@ -1,16 +1,31 @@
 # Course - https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=1&t=0s
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 20218'
+    },
+    {
+        'author': 'Jane',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 20218'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<p><h1>Hello, World!</h1></p>"
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<p><h1>About Page</h1></p>"
+    return render_template('about.html', title = 'About')
 
 
 if __name__ == '__main__':
