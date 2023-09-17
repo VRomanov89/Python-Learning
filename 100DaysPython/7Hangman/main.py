02dd364a242f381e95cc65f69af8a9f0a6e94b8e
Vladimir Romanov <vladromweb@gmail.com>
2023-09-17 19:00:05 -0400

    Day 7 - Done!
    Day 7 - Done!

--- 100DaysPython/7Hangman/main.py
+++ 100DaysPython/7Hangman/main.py
@@ -6,12 +6,25 @@ word_list = ["ardvark", "baboon", "camel"]
 #Randomly choose one word
 chosen_word = random.choice(word_list)
 
-#User will input a letter
-guess = input("Guess a letter:").lower()
+#
+display = []
+for letter in range(len(chosen_word)):
+    display += "_"
 
-#Check word against letter
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+end_of_game = False
+
+while not end_of_game:
+    #User will input a letter
+    guess = input("Guess a letter:").lower()
+
+    #Check word against letter
+    for pos in range(len(chosen_word)):
+        letter = chosen_word[pos]
+        if letter == guess:
+            display[pos] = letter
+
+    print(display)
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
