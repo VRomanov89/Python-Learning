b2611a658893c2fdb1f7fee69f62d4ade0c7163a
Vladimir Romanov <vladromweb@gmail.com>
2023-10-16 20:29:05 -0400

    Lesson 16 Init
    Lesson 16 Init

--- 100DaysPython/16Classes/lesson.py
+++ 100DaysPython/16Classes/lesson.py
@@ -1,9 +1,19 @@
+"""
+
 from turtle import Turtle, Screen
 
 tim = Turtle()
+tim.color("coral")
+tim.shape("turtle")
+tim.forward(100)
 
-print(tim)
 
 my_screen = Screen()
 print(my_screen.canvheight)
 my_screen.exitonclick()
+
+"""
+
+#reference - https://pypi.org/
+
+import prettytable
